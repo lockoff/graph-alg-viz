b099fd82fb29d57922039cbe8ac3d0f0438c4b94
Kiyan Ahmadizadeh <kahmadizadeh@gmail.com>
2015-07-01 20:49:09 -0700

    Adds a graph directive.

--- app/graph/graph.js
+++ app/graph/graph.js
@@ -9,6 +9,13 @@ angular.module('graphAlgViz.graph', ['ngRoute'])
   });
 }])
 
-.controller('GraphCtrl', [function() {
-
+.controller('GraphCtrl', ['$scope', function($scope) {
+    $scope.nodes = [{}, {}, {}, {}, {}];
+    $scope.links = [
+      {source: $scope.nodes[0], target: $scope.nodes[1]},
+      {source: $scope.nodes[1], target: $scope.nodes[2]},
+      {source: $scope.nodes[2], target: $scope.nodes[3]},
+      {source: $scope.nodes[3], target: $scope.nodes[4]},
+      {source: $scope.nodes[4], target: $scope.nodes[0]}
+    ]
 }]);
