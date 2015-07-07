2f74a6698db43e34eb8fb4c8868819147a2288ce
Kiyan Ahmadizadeh <kahmadizadeh@gmail.com>
2015-07-06 23:37:14 -0700

    Adds attributes to force layout directive.

--- app/components/force-layout/force-layout-directive.js
+++ app/components/force-layout/force-layout-directive.js
@@ -3,25 +3,57 @@
 angular.module('graphAlgViz.force-layout.force-layout-directive', [])
 
   .directive('forceLayoutGraph', [function() {
-    var link = function($scope, $el, $attrs) {
-      var sideLength = 500; // Contain graph in a square.
-
+    /*
+     * Validates that an attributes object has properties specifying for width, height, nodes,
+     * and links required for a force layout.
+     */
+    function validateAttributes(attrs) {
+      if (!attrs.width || !attrs.height) {
+        throw "You must specify a width and a height for the graph."
+      }
+      if (!attrs.nodes || !attrs.links) {
+        throw "You must specify the scope attributes containing nodes and links data."
+      }
+    }
+    /*
+     * Gets a new force layout initialized to use a collection of nodes and links, whose options
+     * are configured with the specified attributes.
+     */
+    function getForceLayout(attrs, nodesData, linksData, tickFn) {
       var forceLayout = d3.layout.force()
-        .size([sideLength, sideLength])
-        .nodes($scope.nodes)
-        .links($scope.links)
-        .linkDistance(100)
-        .charge(-60)
-        .on("tick", tick);
+        .on("tick", tickFn)
+        .size([attrs.width, attrs.height])
+        .nodes(nodesData)
+        .links(linksData);
+      function setAttribute(attributeName) {
+        if (attrs[attributeName]) forceLayout[attributeName](attrs[attributeName]);
+      }
+
+      setAttribute("linkDistance");
+      setAttribute("charge");
+      setAttribute("theta");
+      setAttribute("friction");
+      return forceLayout;
+    }
+
+    var link = function($scope, $el, $attrs) {
+      validateAttributes($attrs);
+      var nodesData = $scope[$attrs.nodes];
+      var linksData = $scope[$attrs.links];
+      var forceLayout = getForceLayout($attrs, nodesData, linksData, tick);
 
       var svgSelection = d3.select($el[0])
         .append("svg")
-        .attr({width: sideLength, height: sideLength})
-        .attr("viewBox", "0 0 " + sideLength + " " + sideLength);
+        .attr({width: $attrs.width, height: $attrs.height})
+        .attr("viewBox", "0 0 " + $attrs.width + " " + $attrs.height);
 
       var nodeSelection = svgSelection.selectAll(".node");
       var linkSelection = svgSelection.selectAll(".link");
 
+      /**
+       * Update any nodes or links in the SVG with coordinates matching those assigned by the
+       * force layout. Called on each tick of the force layout simulation.
+       */
       function tick() {
         linkSelection.attr("x1", function (d) { return d.source.x; });
         linkSelection.attr("y1", function (d) { return d.source.y; });
@@ -32,9 +64,12 @@ angular.module('graphAlgViz.force-layout.force-layout-directive', [])
         nodeSelection.attr("cy", function(d) { return d.y; })
       }
 
+      /*
+       * Updates the SVG containing the graph with any new nodes or links that have joined the
+       * graph.
+       */
       function update() {
-        console.log("Update has been called...");
-        linkSelection = linkSelection.data($scope.links);
+        linkSelection = linkSelection.data(linksData);
         linkSelection
           .enter()
           .insert("line", ".node")
@@ -43,7 +78,7 @@ angular.module('graphAlgViz.force-layout.force-layout-directive', [])
           .exit()
           .remove();
 
-        nodeSelection = nodeSelection.data($scope.nodes);
+        nodeSelection = nodeSelection.data(nodesData);
         nodeSelection
           .enter()
           .insert("circle")
