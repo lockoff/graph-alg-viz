15e49eb84e18d135dfc46e96489c84a58facd243
Kiyan Ahmadizadeh <kahmadizadeh@gmail.com>
2015-07-10 21:38:07 -0700

    Improves cumulative average chart.
    + Set chart styling dynamically with graph generation params.
    + Streamline logic with promises when generation graph.

--- app/graph/graph.js
+++ app/graph/graph.js
@@ -14,59 +14,101 @@ angular.module('graphAlgViz.graph', ['ngRoute', 'highcharts-ng', 'graphAlgViz.gr
     $scope.links = [];
     $scope.n = 50;
     $scope.p = 0.05;
-    $scope.maxTrials = 1000;
-    $scope.runningNumEdgesAvg = 0;
+    $scope.maxTrials = 10;
+    $scope.cumulativeAvg = 0;
     $scope.runTrials = runTrials;
+    var averageHistory = [];
 
-    var averageHistory = [61];
     function getExpectedNumEdges() {
-      return (1/2) * $scope.n * ($scope.n - 1) * $scope.p;
+      return (1 / 2) * $scope.n * ($scope.n - 1) * $scope.p;
     }
+
+    function getStdDevNumEdges() {
+      return Math.sqrt((1 / 2) * $scope.n * ($scope.n - 1) * $scope.p * (1 - $scope.p))
+    }
+
     $scope.chartOptions = {
       useHighStocks: false,
       options: {
         chart: {
           type: 'spline',
-          zoomType: 'x'
+          zoomType: 'x',
+          width: 500,
+          height: 500,
+          marginRight: 65
+        },
+        legend: {
+          enabled: false
         }
       },
-      series: [{data: averageHistory}],
+      series: [
+        {
+          data: averageHistory
+        }
+      ],
       title: {
-        text: 'Average Number of Edges Over Trials'
+        text: 'Average Number of Edges Over Generated Graphs'
       },
       yAxis: {
-        min: 50,
-        max: 70
+        title: {
+          text: "Average Number of Edges"
+        },
+        plotLines: [{
+          color: 'red',
+          width: 3,
+          label: {
+            text: "Expected<br>Value",
+            align: "right",
+            textAlign: "left"
+          }
+        }]
+      },
+      xAxis: {
+        title: {
+          text: "Number of Trials"
+        },
+        allowDecimals: false
       },
       loading: false
     };
     var frameDuration = 10;
     var finalFrameDuration = 1000;
 
+    function onGenerationParamChange() {
+      $scope.chartOptions.yAxis.min = getExpectedNumEdges() - 2 * getStdDevNumEdges();
+      $scope.chartOptions.yAxis.max = getExpectedNumEdges() + 2 * getStdDevNumEdges();
+      $scope.chartOptions.yAxis.plotLines[0].value = getExpectedNumEdges();
+      averageHistory.length = 0;
+      $scope.cumulativeAvg = 0;
+      setXAxisBounds();
+    }
+
+    function setXAxisBounds() {
+      $scope.chartOptions.xAxis.min = 1;
+      $scope.chartOptions.xAxis.max = averageHistory.length + $scope.maxTrials;
+    }
+
+    function updateCumulativeAvg() {
+      $scope.cumulativeAvg = ($scope.links.length + averageHistory.length * $scope.cumulativeAvg)
+        / (averageHistory.length + 1);
+      averageHistory.push({x: averageHistory.length + 1, y: $scope.cumulativeAvg});
+    }
+
+    onGenerationParamChange();
+    $scope.$watch('n', onGenerationParamChange);
+    $scope.$watch('p', onGenerationParamChange);
 
     function runTrials() {
-      $scope.runningNumEdgesAvg = 0;
-      var runningNumEdgesSum = 0;
-      function updateNumEdgesAvg(numTrials) {
-        runningNumEdgesSum += $scope.links.length;
-        $scope.runningNumEdgesAvg = runningNumEdgesSum / numTrials;
-        averageHistory.push($scope.runningNumEdgesAvg);
-        //if (averageHistory.length > 10) averageHistory.shift();
-      }
-      function scheduleTrial(numTrials, lastTrialPromise) {
-        return lastTrialPromise.then(function() {
-          if (numTrials > 0) {
-            updateNumEdgesAvg(numTrials);
-          }
-          return generator.generate($scope.nodes, $scope.links, $scope.n, $scope.p, frameDuration, finalFrameDuration);
-        });
-      }
+      setXAxisBounds();
       var deferred = $q.defer();
       var currentPromise = deferred.promise;
       for (var i = 0; i < $scope.maxTrials; i++) {
-        currentPromise = scheduleTrial(i, currentPromise);
+        currentPromise = currentPromise.then(function () {
+          return generator.generate($scope.nodes, $scope.links, $scope.n, $scope.p, frameDuration, finalFrameDuration);
+        }).then(function () {
+          return updateCumulativeAvg();
+        });
       }
-      currentPromise.then(function() { updateNumEdgesAvg($scope.maxTrials); });
       deferred.resolve();
     }
   }]);
